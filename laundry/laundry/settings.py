98bfc110e9a5e40db491b5f50991f2875bc4b3ea
Sarath Kumar <sarathkumar455@gmail.com>
2024-01-10 20:47:07 +0530

    static file prob

--- laundry/laundry/settings.py
+++ laundry/laundry/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-yq5(pn%auo#*w*j%vd9l-(ohe6_xv_s^*3nmmkuomh(2u%-z*z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
